8079b166da2efa2046482a3ff5ba950bfd05fdaf
TSR-2004 <ts7583@srmist.edu.in>
2026-02-17 15:19:18 +0530

    first task completed

--- src/PalindromeMain.java
+++ src/PalindromeMain.java
@@ -1,36 +1,7 @@
-/**
- * ================================================================
- * MAIN CLASS – Palindrome Checker Application
- * ================================================================
- *
- * Use Case 1: Application Entry & Welcome Message
- *
- * Description:
- * This class represents the entry point of the
- * Palindrome Checker Management System.
- *
- * At this stage, the application:
- *  - Starts execution from the main() method
- *  - Displays a welcome message
- *  - Shows application version
- *
- * No palindrome logic is implemented yet.
- * The goal is to establish a clear startup flow.
- *
- * Author: Tanmay Singh
- * Version: 1.0
- */
+
 
 public class PalindromeMain {
 
-    /**
-     * Application entry point.
-     *
-     * This is the first method executed by the JVM
-     * when the program starts.
-     *
-     * @param args Command-line arguments
-     */
     public static void main(String[] args) {
 
         System.out.println("Welcome to the Palindrome Checker Management System");
